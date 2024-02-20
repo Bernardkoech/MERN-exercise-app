adc0a66e178cc7bac58ea987fba6bf614895f033
Bernard <bernard.koech@student.moringaschool.com>
2024-02-20 15:01:51 +0300

    Complete

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': "http://localhost:5555"
+      '/api': "https://mern-exercise-app.vercel.app"
     }
   },
   plugins: [react()],
